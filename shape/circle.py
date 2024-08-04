a11f676e5106e3cf845c98ea7159b8e327c6e7eb
RyanLim4 <rrl23@ic.ac.uk>
2024-08-04 22:04:45 +0800

    completed

---
+++ shape/circle.py
@@ -0,0 +1,14 @@
+class Circle:
+    def __init__(self, center, radius):
+        self.center = center
+        self.radius = radius
+    
+    def __contains__(self, point):
+        from math import sqrt
+        # find dist between point and center
+        dist = sqrt((point[0] - self.center[0])**2 + (point[1] - self.center[1])**2)
+        if dist < self.radius:
+            return True
+        else:
+            return False
+        
